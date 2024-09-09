65eaa3eab01706605c1fa3abdebe0e7a1cf635b7
QusaiAlqattan <qusaikattan.jb@gmail.com>
2024-09-09 19:15:24 +0300

    after deleting analysis repo

--- analysis/src/main/java/com/example/analysis/repo/AnalysedNumbersRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.analysis.repo;
-
-import com.example.mysqlreader.model.YourData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface YourDataRepository extends MongoRepository<Data, String> {
-}
